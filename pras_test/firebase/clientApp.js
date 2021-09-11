7ca1d0e725a26e1c6839ee1277c5801c8f5aeb5b
prashathc <prashathc@gmail.com>
2021-09-11 16:43:11 -0400

    got the authentication functionality to work

--- pras_test/firebase/clientApp.js
+++ pras_test/firebase/clientApp.js
@@ -1,9 +1,8 @@
 import { initializeApp, getApps } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -13,34 +12,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-const db = getFirestore(app)
 const auth = getAuth(app)
 
-export {db, auth}
+export {auth}
 
 
-
-// // Import the functions you need from the SDKs you need
-// import firebase from "firebase/app";
-// import "firebase/auth";
-// import "firebase/firestore";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-
-// // Your web app's Firebase configuration
-
-// const clientCredentials = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
-// // Initialize Firebase
-
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(clientCredentials);
-// }
-// export default firebase;
